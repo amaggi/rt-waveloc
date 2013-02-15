67a144cec5cf58899610d0a932e16d82ac044011
Alessia Maggi <alessia.maggi@unistra.fr>
2013-02-15 23:23:36 +0100

    Added return of time-shift of gaussian filter

--- rtwl/am_signal.py
+++ rtwl/am_signal.py
@@ -12,10 +12,13 @@ def gaussian_filter(sigma_f, f0, dt):
     # calculate the number of points required for +/- 3*sigma_t
     npts_1sigma = int(sigma_t/dt)
     npts = npts_1sigma*8 + 1 
+    # calculate the time-shift of this filter
+    tshift=((npts-1)/2) * dt
     # set up the t-array
     t=np.arange(npts)*dt - 4*npts_1sigma*dt 
 
     gauss = 1/(sqrt(2*pi)*sigma_f) * exp(-t*t/(2*sigma_t*sigma_t)) * \
             exp(complex(0,1)*2*pi*f0*t)
 
-    return gauss
+
+    return gauss, tshift
