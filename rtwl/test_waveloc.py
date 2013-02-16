8ed8311ec8e221dfea9629ea04ab14b5ee1554f3
Alessia Maggi <alessia.maggi@unistra.fr>
2013-02-16 14:45:01 +0100

    Modified waveloc test to include recently added tests

--- rtwl/test_waveloc.py
+++ rtwl/test_waveloc.py
@@ -19,13 +19,15 @@ class SetupTests(unittest.TestCase):
 
 if __name__ == '__main__':
 
-  import test_processing
+  import test_processing, test_nllstuff, test_hdf5
   import logging
   logging.basicConfig(level=logging.INFO, format='%(levelname)s : %(asctime)s : %(message)s')
  
 
   suite_list=[suite(),
     test_processing.suite(),
+    test_nllstuff.suite(),
+    test_hdf5.suite(),
     ]
 
   alltests=unittest.TestSuite(suite_list)
