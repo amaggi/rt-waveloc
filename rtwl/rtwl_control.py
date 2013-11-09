b804cfcaa7567725b702d1db855af095d22154cc
Alessia Maggi <alessia.maggi@unistra.fr>
2013-11-09 15:13:29 +0100

    Added serialisation of traces to the raw_data exchange

--- rtwl/rtwl_control.py
+++ rtwl/rtwl_control.py
@@ -1,5 +1,7 @@
 import optparse
 import logging
+import pika
+from cPickle import dumps, loads
 from options import RtWavelocOptions
 from rtwl_io import readConfig
 
@@ -7,13 +9,20 @@ def rtwlStart():
     """
     Starts rtwl. Configuration is read from 'rtwl.config' file.
     """
-   
-    
+      
     config_file='rtwl.config'
     
     # read options into waveloc options objet
     wo=RtWavelocOptions()
     wo.opdict=readConfig(config_file)
+    
+    # set up rabbitmq
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+                        host='localhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='raw_data',
+                         type='direct')
     
     if wo.run_offline:
         # Run in offline mode
@@ -27,17 +36,45 @@ def rtwlStart():
         for name in fnames:
             st=read(name)
             obs_list.append(st[0])
+        nsta=len(obs_list)
     
-    
+        # use data to set dt in waveloc options
+        dt=obs_list[0].stats.delta
+        wo.opdict['dt'] = dt
+
+        # split data files to simulate packets of real-time data
+        obs_split=[]
+        for obs in obs_list:
+            split = obs / 3
+            obs_split.append(split)
+        ntr=len(obs_split[0])
+        
+        for itr in xrange(ntr):
+            # publish data to "seiscomp" stream
+            for ista in xrange(nsta):
+                tr = obs_split[ista][itr]
+                sta=tr.stats.station
+                # The message is just a pickled version of the trace
+                message=dumps(tr,-1)
+                channel.basic_publish(exchange='raw_data',
+                            routing_key=sta,
+                            body=message,
+                            properties=pika.BasicProperties(delivery_mode=2,)
+                            )
+                tr_test=loads(message)
+                print " [x] Sent %r:%r" % (sta, "New data (%d) for station %s"
+                                                %(itr,tr_test.stats.station))
+        
     
     else :
         # Run in true real-time mode
         raise NotImplementedError()
     
-    
+    connection.close()
     
 if __name__=='__main__':
 
+    # parse command line
     p=optparse.OptionParser()
 
     p.add_option('--start',action='store_true',help="start rtwl")
@@ -50,5 +87,8 @@ if __name__=='__main__':
     else:
         logging.basicConfig(level=logging.INFO, format='%(levelname)s : %(asctime)s : %(message)s')
 
+   
+
     if options.start:
-            rtwlStart()
+            rtwlStart() # start process
+            
