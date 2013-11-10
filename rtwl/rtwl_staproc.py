4faae97b392b58ee75bab15b854be37a411c8967
Alessia Maggi <alessia.maggi@unistra.fr>
2013-11-11 00:36:46 +0100

    Basic multiproc station version working

--- rtwl/rtwl_staproc.py
+++ rtwl/rtwl_staproc.py
@@ -19,26 +19,30 @@ rt_dict['dx2']=(am_rt_signal.dx2,2)
 ###############
 
 class rtwlStaProcessor(object):
-    def __init__(self,wo,sta) :
+    def __init__(self,wo) :
         
         # basic parameters
         self.wo = wo
-        self.sta = sta
+        self.sta_list = wo.sta_list
         self.max_length = self.wo.opdict['max_length']
         self.safety_margin = self.wo.opdict['safety_margin']
         self.dt = self.wo.opdict['dt']
         
-        # real-time stream for processing
-        self.rtt = RtTrace(max_length=self.max_length)
+        # real-time streams for processing
+        self.rtt={}
+        for sta in self.sta_list:
+            self.rtt[sta] = RtTrace(max_length=self.max_length)
         self._register_preprocessing()
         
-        # queue setup
-        self.connection, self.channel = _setupRabbitMQ()
         
         # independent process (for parallel calculation)
-        self.p = multiprocessing.Process(name='proc_%s'%sta,
-                                       target=self._do_proc)
-        self.p.start()
+        for sta in self.sta_list:
+            p = multiprocessing.Process(
+                                        name='proc_%s'%sta,
+                                        target=self._do_proc,
+                                        args=(sta,)
+                                        )
+            p.start()
         
     def _register_preprocessing(self):
         wo=self.wo
@@ -46,7 +50,8 @@ class rtwlStaProcessor(object):
         # if this is a synthetic
         if wo.is_syn:
             # do dummy processing only
-            self.rtt.registerRtProcess('scale', factor=1.0)
+            for tr in self.rtt.values():
+                tr.registerRtProcess('scale', factor=1.0)
 
         else:
             # get gaussian filtering parameters
@@ -56,49 +61,54 @@ class rtwlStaProcessor(object):
             # for now just use one window
             kwin = wo.opdict['kwin']
             # register pre-processing of data here
-            self.rtt.registerRtProcess('convolve', conv_signal=gauss)
-            self.rtt.registerRtProcess('sw_kurtosis', win=kwin)
-            self.rtt.registerRtProcess('boxcar', width=50)
-            self.rtt.registerRtProcess('differentiate')
-            self.rtt.registerRtProcess('neg_to_zero')
+            for tr in self.rtt.values():
+                tr.registerRtProcess('convolve', conv_signal=gauss)
+                tr.registerRtProcess('sw_kurtosis', win=kwin)
+                tr.registerRtProcess('boxcar', width=50)
+                tr.registerRtProcess('differentiate')
+                tr.registerRtProcess('neg_to_zero')
             
-    def _do_proc(self):
+    def _do_proc(self,sta):
         proc_name = multiprocessing.current_process().name
-    
+ 
+        #queue setup
+        connection, channel = _setupRabbitMQ()
+   
         # bind to the raw data 
-        result = self.channel.queue_declare(exclusive=True)
+        result = channel.queue_declare(exclusive=True)
         queue_name = result.method.queue
-        #print proc_name, queue_name
-        self.channel.queue_bind(exchange='raw_data', 
+       
+        channel.queue_bind(exchange='raw_data', 
                                     queue=queue_name, 
-                                    routing_key=self.sta)
-        consumer_tag=self.channel.basic_consume(
+                                    routing_key=sta)
+        consumer_tag=channel.basic_consume(
                                     self._callback_proc,
                                     queue=queue_name,
                                     #no_ack=True
                                     )
         #print proc_name, consumer_tag
-        self.channel.basic_qos(prefetch_count=1)
+        channel.basic_qos(prefetch_count=1)
         try:
-            self.channel.start_consuming()
+            channel.start_consuming()
         except UserWarning:
             logging.log(logging.INFO,"Received UserWarning from %s"%proc_name)       
-            self.channel.basic_cancel(consumer_tag)
+            channel.basic_cancel(consumer_tag)
 
     def _callback_proc(self, ch, method, properties, body):
         if body=='STOP':
             # acknowledge
             ch.basic_ack(delivery_tag = method.delivery_tag)
-            self.channel.stop_consuming()
+            ch.stop_consuming()
             # send on to next exchange
-            self.channel.basic_publish(exchange='proc_data',
-                            routing_key=self.sta,
+            ch.basic_publish(exchange='proc_data',
+                            routing_key=method.routing_key,
                             body=body,
                             properties=pika.BasicProperties(delivery_mode=2,)
                             )
             raise UserWarning
         else:
             # unpack data packet
+            sta=method.routing_key
             tr=loads(body)
             
             # pre-correct for filter_shift
@@ -109,18 +119,18 @@ class rtwlStaProcessor(object):
             
             # append trace from message to real-time trace
             # this automatically triggers the rt processing
-            pp_data = self.rtt.append(tr, gap_overlap_check = True)
+            pp_data = self.rtt[sta].append(tr, gap_overlap_check = True)
             
             # send the processed data on to the proc_data exchange
             message=dumps(pp_data,-1)
-            self.channel.basic_publish(exchange='proc_data',
-                            routing_key=self.sta,
+            ch.basic_publish(exchange='proc_data',
+                            routing_key=method.routing_key,
                             body=message,
                             properties=pika.BasicProperties(delivery_mode=2,)
                             )
             logging.log(logging.INFO,
                             " [S] Sent %r:%r" % 
-                            (self.sta, "Processed data for station %s"
+                            (method.routing_key, "Processed data for station %s"
                             %(pp_data.stats.station)))
             
             # signal to the raw_data exchange that have finished with data
@@ -156,10 +166,7 @@ def rtwlStart(wo):
     p_info.start()
     
     # set up processes to process data
-    p_list=[]
-    for sta in wo.sta_list:
-        p=rtwlStaProcessor(wo,sta)
-        p_list.append(p)
+    rtwlStaProcessor(wo)
     
 def receive_info():
     proc_name = multiprocessing.current_process().name
