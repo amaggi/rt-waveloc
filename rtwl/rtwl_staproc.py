896438c38ec7b27ac01a756fe953d9b553e7c175
Alessia Maggi <alessia.maggi@unistra.fr>
2013-11-10 10:59:29 +0100

    Real-time processing completed

--- rtwl/rtwl_staproc.py
+++ rtwl/rtwl_staproc.py
@@ -1,31 +1,81 @@
 import logging, pika,time
 import multiprocessing
+import numpy as np
+import am_rt_signal
+from obspy.realtime import RtTrace
+from am_signal import gaussian_filter
 from cPickle import dumps, loads
 from rtwl_io import rtwlGetConfig, rtwlParseCommandLine
 
+###############
+# stuff that needs doing while I still have 
+# realtime functions that are not part of obspy
+import obspy.realtime
+rt_dict= obspy.realtime.rttrace.REALTIME_PROCESS_FUNCTIONS
+rt_dict['neg_to_zero']=(am_rt_signal.neg_to_zero,0)
+rt_dict['convolve']=(am_rt_signal.convolve,1)
+rt_dict['sw_kurtosis']=(am_rt_signal.sw_kurtosis,1)
+rt_dict['dx2']=(am_rt_signal.dx2,2)
+###############
+
 class rtwlStaProcessor(object):
     def __init__(self,wo,sta) :
+        
+        # basic parameters
         self.wo = wo
         self.sta = sta
+        self.max_length = self.wo.opdict['max_length']
+        self.safety_margin = self.wo.opdict['safety_margin']
+        self.dt = self.wo.opdict['dt']
+        
+        # real-time stream for processing
+        self.rtt = RtTrace(max_length=self.max_length+self.safety_margin)
+        self._register_preprocessing()
+        
+        # queue setup
         self.connection, self.channel = _setupRabbitMQ()
+        
+        # independent process (for parallel calculation)
         self.p = multiprocessing.Process(name='proc_%s'%sta,
                                        target=self._do_proc)
         self.p.start()
         
+    def _register_preprocessing(self):
+        wo=self.wo
+        
+        # if this is a synthetic
+        if wo.is_syn:
+            # do dummy processing only
+            self.rtt.registerRtProcess('scale', factor=1.0)
+
+        else:
+            # get gaussian filtering parameters
+            f0, sigma, dt = wo.gauss_filter
+            gauss, self.filter_shift = gaussian_filter(f0, sigma, dt)
+            # get kwin
+            # for now just use one window
+            kwin = wo.opdict['kwin']
+            # register pre-processing of data here
+            self.rtt.registerRtProcess('convolve', conv_signal=gauss)
+            self.rtt.registerRtProcess('sw_kurtosis', win=kwin)
+            self.rtt.registerRtProcess('boxcar', width=50)
+            self.rtt.registerRtProcess('differentiate')
+            self.rtt.registerRtProcess('neg_to_zero')
+            
     def _do_proc(self):
         proc_name = multiprocessing.current_process().name
     
         # bind to the raw data 
         result = self.channel.queue_declare(exclusive=True)
         queue_name = result.method.queue
-        print proc_name, queue_name
+        #print proc_name, queue_name
         self.channel.queue_bind(exchange='raw_data', 
                                     queue=queue_name, 
                                     routing_key=self.sta)
         consumer_tag=self.channel.basic_consume(
                                     self._callback_proc,
                                     queue=queue_name)
-        print proc_name, consumer_tag
+        #print proc_name, consumer_tag
         self.channel.basic_qos(prefetch_count=1)
         try:
             self.channel.start_consuming()
@@ -38,8 +88,32 @@ class rtwlStaProcessor(object):
             ch.basic_ack(delivery_tag = method.delivery_tag)
             raise UserWarning
         else:
+            # unpack data packet
             tr=loads(body)
-            print tr.stats.station
+            
+            # pre-correct for filter_shift
+            tr.stats.starttime -= self.filter_shift
+            
+            # make dtype of data float if it is not already
+            tr.data=tr.data.astype(np.float32)
+            
+            # append trace from message to real-time trace
+            # this automatically triggers the rt processing
+            pp_data = self.rtt.append(tr, gap_overlap_check = True)
+            
+            # send the processed data on to the proc_data exchange
+            message=dumps(pp_data,-1)
+            self.channel.basic_publish(exchange='proc_data',
+                            routing_key=self.sta,
+                            body=message,
+                            properties=pika.BasicProperties(delivery_mode=2,)
+                            )
+            logging.log(logging.INFO,
+                            " [S] Sent %r:%r" % 
+                            (self.sta, "Processed data for station %s"
+                            %(pp_data.stats.station)))
+            
+            # signal to the raw_data exchange that have finished with data
             ch.basic_ack(delivery_tag = method.delivery_tag)
         
 def _setupRabbitMQ():
@@ -50,9 +124,9 @@ def _setupRabbitMQ():
     channel = connection.channel()
     
     # set up exchanges for data and info
-    channel.exchange_declare(exchange='raw_data',type='direct')
-    channel.exchange_declare(exchange='info',    type='fanout')
-    channel.exchange_declare(exchange='proc_data',type='direct')
+    channel.exchange_declare(exchange='raw_data',exchange_type='direct')
+    channel.exchange_declare(exchange='info',    exchange_type='fanout')
+    channel.exchange_declare(exchange='proc_data',exchange_type='direct')
     
     return connection, channel   
  
