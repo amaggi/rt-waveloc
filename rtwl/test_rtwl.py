143e1eaa5ba4c6c74670c80f90d8be736bdd636a
Alessia Maggi <alessia.maggi@unistra.fr>
2013-11-11 15:05:16 +0100

    Added test for stations in staproc

--- rtwl/test_rtwl.py
+++ rtwl/test_rtwl.py
@@ -1,23 +1,59 @@
 import unittest
+import multiprocessing
+import os
+from cPickle import dump, load
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(SyntheticMigrationTests('test_rt_migration_true'))
+    suite.addTest(SyntheticProcessingTests('test_syn_staproc'))
     return suite
     
-class SyntheticMigrationTests(unittest.TestCase):
+class SyntheticProcessingTests(unittest.TestCase):
 
     def setUp(self):
-        pass
-       
-    def test_rt_migration_true(self):
-        self.assertTrue(True)
- 
+        from rtwl_io import rtwlGetConfig
+        # read config file
+        self.wo = rtwlGetConfig('test_data/test_rtwl_syn.config')
+        self.sp = None
+        
+    
+    def _run_staproc(self, wo, filename):
+        from rtwl_staproc import rtwlStaProcessor
+        
+        sp = rtwlStaProcessor(wo)
+        # save stuff to check out to a file
+        f=open(filename,'w')
+        dump(sp.rtt,f,-1)
+        f.close()
+               
+          
+    def test_syn_staproc(self):
+        from rtwl_control import rtwlStart, rtwlStop
+        
+        tmpfile = 'test_syn_staproc.tmp'
+        
+        p=multiprocessing.Process(target=self._run_staproc, args=(self.wo, tmpfile))
+        p.start()
+        
+        rtwlStart(self.wo)        
+        rtwlStop(self.wo)
+        
+        p.join()
+        
+        f=open(tmpfile,'r')
+        rtt=load(f)
+        f.close()
+        sta_list=self.wo.sta_list
+        for sta in sta_list:
+            self.assertTrue(rtt.has_key(sta))
+        
+        os.remove(tmpfile)
+        
 
 if __name__ == '__main__':
 
-#  import logging
-#  logging.basicConfig(level=logging.INFO, format='%(levelname)s : %(asctime)s : %(message)s')
+    import logging
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s : %(asctime)s : %(message)s')
  
-  unittest.TextTestRunner(verbosity=2).run(suite())
+    unittest.TextTestRunner(verbosity=2).run(suite())
  
